7cabea2ec3994eef68d3f036d11d5e5343186823
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:52:04 +0330

    Add project files.

---
+++ Exercise16/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise16
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 0 or 1:");
+            string a = Console.ReadLine();
+            var result1 = Convert.ToInt32(a);
+            var result2 = Comparision(result1);
+            Console.WriteLine("Result is:" + result2);
+        }
+
+        public static int Comparision(int a)
+        {
+            if (a == 0)
+                return 1;
+            else if (a == 1)
+                return 0;
+            else
+                return a;
+        }
+    }
+
+}
